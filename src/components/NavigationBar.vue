9a3b1ad22d1fc598c35d2f317619e8eb90939abe
andrew <231880449@smail.nju.edu.cn>
2025-04-02 14:14:16 +0800

    上传框架

---
+++ src/components/NavigationBar.vue
@@ -0,0 +1,44 @@
+<script setup>
+import { ElMenu, ElMenuItem } from "element-plus";
+</script>
+
+<template>
+  <div class="nav-bar-container">
+    <el-menu
+        class="nav-bar"
+        mode="horizontal"
+        :router="true"
+    >
+      <el-menu-item index="0">
+        <img
+            style="width: 100px"
+            src="/Users/andrew/Downloads/NailongMall/tomatomall-frontend/src/images/NailongLogo.png"
+            alt="Element logo"
+        />
+      </el-menu-item>
+      <el-menu-item index="/">番茄书城</el-menu-item>
+      <el-menu-item index="/cart">购物车</el-menu-item>
+      <el-menu-item index="/warehouse">库存管理</el-menu-item>
+      <el-menu-item index="/userinfo">个人信息</el-menu-item>
+    </el-menu>
+  </div>
+</template>
+
+<style scoped>
+.nav-bar-container {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  z-index: 1000;
+  height: 60px;
+}
+
+.nav-bar {
+  display: inline-block;
+  width: 100%;
+  margin: 0 auto;
+  height: 100%;
+}
+
+</style>
